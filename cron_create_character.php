32a5dd003d82ae6f611ee6d57edac285da8906e9
VivvMich <mvicorp@hotmail.fr>
2024-04-12 09:33:50 +0200

    add log filer maker

--- cron_create_character.php
+++ cron_create_character.php
@@ -3,9 +3,11 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 require_once 'vendor/autoload.php';
+require_once 'tools.php';
 
 $pdo = new PDO("mysql:host=localhost;dbname=nosql", "formateur", "1234");
 
+// API Character creator;
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://randomuser.me/api/?results=1");
@@ -17,20 +19,36 @@ $res = json_decode($response, true);
 $fake_firstname = $res["results"][0]["name"]["first"];
 $fake_lastname = $res["results"][0]["name"]["last"];
 
-$age = rand(18, 106);
+// NO SQL, Curse Object
+try {
+    $client = new \MongoDB\Client("mongodb://localhost:27017");
+    $database = $client->selectDatabase('object');
+    $collection = $database->selectCollection('curse_object');
 
-$sql = "INSERT INTO owner(firstname_owner, lastname_owner, age_owner, object_owner, curse_object_owner) VALUES (?,?,?,?,?)";
-$stmt = $pdo->prepare($sql);
-$stmt->execute([$fake_firstname, $fake_lastname, $age, null, null]);
+    $objects = $collection->find()->toArray();
+    $nbr_objects = count($objects);
+    $name_objects = $objects[rand(0, $nbr_objects - 1)]['name'];
+    $curse_objects = $objects[rand(0, $nbr_objects - 1)]['curse'];
+
+    $age = rand(18, 106);
+
+    $sql = "INSERT INTO owner(firstname_owner, lastname_owner, age_owner, object_owner, curse_object_owner) VALUES (?,?,?,?,?)";
+    $stmt = $pdo->prepare($sql);
+    if( $stmt->execute([$fake_firstname, $fake_lastname, $age, $name_objects, $curse_objects])) {
+    addLog("The cursed object " . $name_objects . " was given to " . $fake_firstname . " " . $fake_lastname . ".");
+    }else{
+        addLog("Error - The cursed object the cursed object could not have been given");
+    }
+
+}catch (Exception $e) {
+    addLog($e->getMessage());
+}
+
+
+
 
 // * * * * * /usr/bin/php '/var/www/html/NOSQL/cron_create_character.php'
 
 
-$client = new \MongoDB\Client("mongodb://localhost:27017");
-$database = $client->selectDatabase('object');
-$collection = $database->selectCollection('curse_object');
 
-$objects = $collection->find()->toArray();
-$nbr_objects = count($objects);
 
-var_dump($objects[rand(0, $nbr_objects)]);
--- tools.php
+++ tools.php
@@ -14,26 +14,12 @@ function sendMessage(string $message, string $status, string $location, int|null
 
 }
 
-/**
- * Cette fonction va créer des inputs radio à partir 
- * de la base de données.
- * CollumName -> Correspond au nom de la colonne dans la BDD.
- * dBValue -> Correspond à la valeur venu de la BDD pour selectioner l'input radio parmis les $values.
- */
-function createCheckButton(string $collumnName, string $dBValue, array $values) : string {
-    $result = '';
-    foreach ($values as $value) {
-    $result .= "<div class='form-check'>";
-    $upper = ucfirst($value);
-        if($value === $dBValue){
-            $result .= "<input class='form-check-input' type='radio' name='$collumnName' checked value='$value' id=''>";
-        }else{
-            $result .= "<input class='form-check-input' type='radio' name='$collumnName' value='$value' id=''>";
-        }
-    
-    $result .= "<label class='form-check-label' for='$collumnName'>$upper</label></div>";
-    }
-    return $result;
+function addLog(String $message) {
+    $file = fopen("theCurseLog.txt", "a") or die("Impossible d'ouvrir le fichier.");
+    $dateObj = new DateTime();
+    $date = $dateObj->format('d-m-Y H:i:s');
+    $fullMessage = $date . " : " . $message . "\n";
+    fwrite($file, $fullMessage);
 }
 
 
